72f6ad8e0459ec4cfb880cd239ad06fd5a720eed
Dilshan <ndkumarawansha@gmail.com>
2024-03-16 10:44:42 +0530

    cart bean and interface added

--- ejb/src/main/java/ejb/impl/CartBean.java
+++ ejb/src/main/java/ejb/impl/CartBean.java
@@ -1,4 +1,37 @@
 package ejb.impl;
 
-public class CartBean {
+import ejb.remort.Cart;
+import jakarta.ejb.Stateful;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+@Stateful
+public class CartBean implements Cart {
+    private Map<String, Object> items = new HashMap<>();
+    @Override
+    public void addItem(String item) {
+        items.put(item, item);
+    }
+
+    @Override
+    public int getCount() {
+        return items.size();
+    }
+
+    @Override
+    public double getTotal() {
+        return items.size();
+    }
+
+    @Override
+    public void removeItem(String item) {
+        items.remove(item);
+    }
+
+    @Override
+    public Map getItems() {
+        return items;
+    }
 }
